852f69f2a42e7877470a1e7cc757fa1323ad0b93
Fien van Wetten <fienvw@gmail.com>
2022-12-17 10:38:38 +0100

    sfasf

--- social_format.py
+++ social_format.py
@@ -10,13 +10,29 @@ import leidenalg as la
 
 fh=open('data.moreno_highschool_highschool')
 G=nx.read_weighted_edgelist(fh,comments='%',delimiter=' ',create_using=nx.DiGraph)
+print(G.number_of_nodes())
 G = ig.Graph.from_networkx(G)
-#print(G.number_of_nodes())
+print(G)
 partition = la.find_partition(G, la.ModularityVertexPartition)
 
+for i in range(len(partition)):
+	subgraph = ig.Graph.subgraph(G, partition[i])
+	print(subgraph)
+
+
 print(partition)
+sort_part=[]
+for i in range(len(partition)):
+	tempy=[]
+	for j in range(len(partition[i])):
+		d = G.degree(partition[i][j],mode="out")
+		tempy.append((partition[i][j],d))
 
-
+	tempy = sorted(tempy,reverse=True, key=lambda x: x[1])
+	sort_part.append(tempy)
+		#print(G.degree(partition[i][j],mode="in"))
+for element in sort_part:
+	print(element)
 
 #coms = sorted(nx_comm.louvain_communities(G),key=len,reverse=True)
 #print({len(coms)})
